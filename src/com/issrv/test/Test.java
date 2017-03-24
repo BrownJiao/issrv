635b83663a83f38c8420ccdb47fff5701dc9ddf7
brown <532070061.com>
2017-03-24 14:31:04 +0800

    remove new gitignore directory

--- src/com/issrv/test/Test.java
+++
@@ -1,9 +0,0 @@
-package com.issrv.test;
-
-public class Test {
-
-    public static void main(String[] args) {
-        Singleton.getInstance().sendMail("jiaowenzhe6@163.com", "卧榻之侧，岂能容他人酣睡！");
-    }
-
-}
