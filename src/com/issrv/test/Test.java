bde93a74e793c6b696505487a333d07d653cba27
brown <532070061.com>
2017-03-24 13:48:10 +0800

    提交文件

---
+++ src/com/issrv/test/Test.java
@@ -0,0 +1,9 @@
+package com.issrv.test;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Singleton.getInstance().sendMail("jiaowenzhe6@163.com", "卧榻之侧，岂能容他人酣睡！");
+    }
+
+}
---
+++ src/com/issrv/test/TestMitiThread.java
@@ -0,0 +1,11 @@
+package com.issrv.test;
+
+public class TestMitiThread {
+    
+    public static void main(String[] rags) {
+        System.out.println(Thread.currentThread().getName() + " 线程运行开始!");
+        new MitiSay("A").start();
+        new MitiSay("B").start();
+        System.out.println(Thread.currentThread().getName() + " 线程运行结束!");
+    }
+}
